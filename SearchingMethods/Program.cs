b3ec049343b57cc59e37c4439ab4ee87f7e2d247
Overial <dfjx2201@yandex.ru>
2021-10-20 21:25:55 +0300

    [C#] Created project prototype

--- SearchingMethods/Program.cs
+++ SearchingMethods/Program.cs
@@ -4,6 +4,11 @@ namespace SearchingMethods
 {
     class Program
     {
+        static void BreadthFirstSearch()
+        {
+
+        }
+
         static void DepthFirstSearch()
         {
 
@@ -12,28 +17,27 @@ namespace SearchingMethods
         static void Main(string[] args)
         {
             // User input
-            Console.Write("Enter rows amount: ");
-            int rows = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Enter cols amount: ");
-            int cols = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter matrix dimension: ");
+            int dimension = Convert.ToInt32(Console.ReadLine());
 
             // Creating adjacency matrix
             Random rnd = new Random();
-            int[,] iAdjacencyMatrix = new int[rows, cols];
-            for (int i = 0; i < rows; ++i)
+            int[,] adjacencyMatrix = new int[dimension, dimension];
+            for (int i = 0; i < dimension; ++i)
             {
-                for (int j = 0; j < cols; ++j)
+                for (int j = 0; j < dimension; ++j)
                 {
-                    iAdjacencyMatrix[i, j] = rnd.Next(0, 9);
+                    // Filling matrix with random integers for debugging
+                    adjacencyMatrix[i, j] = rnd.Next(0, 9);
                 }
             }
 
-            // Printing adjancency matrix
-            for (int i = 0; i < rows; ++i)
+            // Printing adjacency matrix
+            for (int i = 0; i < dimension; ++i)
             {
-                for (int j = 0; j < cols; ++j)
+                for (int j = 0; j < dimension; ++j)
                 {
-                    Console.Write(iAdjacencyMatrix[i, j] + "|");
+                    Console.Write(adjacencyMatrix[i, j] + "|");
                 }
                 Console.WriteLine();
             }
